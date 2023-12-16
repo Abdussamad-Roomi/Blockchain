258a6e1a0704a1d05c19c4418e5085adb1f7a93a
Abdussamad-Roomi <84172805+Abdussamad-Roomi@users.noreply.github.com>
2023-12-16 10:51:42 +0500

    refund auction.sol

--- auction.sol
+++ auction.sol
@@ -1,124 +1,91 @@
-//SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.0;
 
-pragma solidity >= 0.8.0;
-
- contract auction {
-    address payable  owner;
-    address highestBidder;
-    uint256 startingTime;
-    uint256 endingTime;
-    event LogUint(uint number);
+contract Auction {
+    address payable public owner;
+    address public highestBidder;
+    uint256 public startingTime;
+    uint256 public endingTime;
+    event LogUint(uint256 number);
     mapping(address => uint256) public bids;
-    uint highestBindingBid;
+    uint256 public highestBindingBid;
     address[] public bidKeys;
-    
 
-
-    
-    function placeBids() payable  external {
-    
+    function placeBid() external payable {
         require(block.timestamp >= startingTime, "Auction has not started");
         require(block.timestamp < endingTime, "Auction has ended");
         require(msg.value > highestBindingBid, "Bid is not high enough");
 
+        // Refund previous bidders with lower bids
+        for (uint256 i = 0; i < bidKeys.length; i++) {
+            address payable bidder = payable(bidKeys[i]);
+            uint256 amount = bids[bidder];
 
-
-        bool check = false;
-
-        if (msg.value > bids[highestBidder]) {
-
-             for (uint i = 0; i < bidKeys.length; i++) {
-                if (bidKeys[i] == msg.sender) {
-                check = true;
-                break;
+            if (amount > 0 && amount < msg.value) {
+                bids[bidder] = 0;
+                bidder.transfer(amount);
             }
         }
-        if(check == true){
-            highestBidder = msg.sender;
-            bids[msg.sender] = msg.value;
-            highestBindingBid = msg.value;
-        }
-        else{
-            bidKeys.push(msg.sender);
-            highestBidder = msg.sender;
-            bids[msg.sender] = msg.value;
-            highestBindingBid = bids[highestBidder];
-        }
 
-            
-        }
+        highestBidder = msg.sender;
+        bids[msg.sender] = msg.value;
+        highestBindingBid = msg.value;
+        bidKeys.push(msg.sender);
     }
-    
 
-    function currentHigestBid() public  view returns (uint256) {
+    function getCurrentHighestBid() public view returns (uint256) {
         return highestBindingBid;
-
     }
 
-
-   function setStartTime(uint time) private  OnlyOwner{
+    function setStartTime(uint256 time) private onlyOwner {
         startingTime = time;
-   }
-
-
-   function setEndTime(uint time) private  OnlyOwner{
-        endingTime = time;
-   }
-    constructor(uint _startTime, uint _endTime) public  {
-            owner = payable(msg.sender);
-            setEndTime(_endTime);
-            setStartTime(_startTime);
-         
-        }
-
-
-
-
-
-   modifier  OnlyOwner() {
-        require(msg.sender == owner);
-        //require(block.timestamp > startingTime);
-    
-    _;
-   }
-
-
-
-  function cancelAuction() external payable  OnlyOwner {
-        require(block.timestamp < endingTime, "Auction already ended");
-     
-        for (uint i = 0; i < bidKeys.length; i++) {
-        address payable bidder = payable(bidKeys[i]);
-        uint256 amount = bids[bidder];
-        
-
-        emit LogUint(amount);
-
-        if (amount > 0) {
-            bids[bidder] = 0;
-            bidder.transfer(amount);
-        }
     }
+
+    function setEndTime(uint256 time) private onlyOwner {
+        endingTime = time;
+    }
+
+    constructor(uint256 _startTime, uint256 _endTime) {
+        owner = payable(msg.sender);
+        setEndTime(_endTime);
+        setStartTime(_startTime);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the owner");
+        _;
+    }
+
+    function cancelAuction() external onlyOwner {
+        require(block.timestamp < endingTime, "Auction already ended");
+
+        for (uint256 i = 0; i < bidKeys.length; i++) {
+            address payable bidder = payable(bidKeys[i]);
+            uint256 amount = bids[bidder];
+
+            emit LogUint(amount);
+
+            if (amount > 0) {
+                bids[bidder] = 0;
+                bidder.transfer(amount);
+            }
+        }
 
         selfdestruct(owner);
-    }  
- function finalizeAuction() external payable  OnlyOwner {
+    }
+
+    function finalizeAuction() external onlyOwner {
         require(block.timestamp >= endingTime, "Auction not yet ended");
-        
-        // Refund the remaining bids to the bidders
-     
-           for (uint i = 0; i < bidKeys.length; i++) {
+
+        for (uint256 i = 0; i < bidKeys.length; i++) {
             address payable bidder = payable(bidKeys[i]);
-            uint amount = bids[bidder];
+            uint256 amount = bids[bidder];
             if ((amount > 0) && (bidder != highestBidder)) {
                 bids[bidder] = 0;
                 bidder.transfer(amount);
-                
-
             }
         }
-     
+
         selfdestruct(owner);
     }
-
- }
+}
